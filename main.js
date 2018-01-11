86708bdb6b7881a4d726aef4a52c07fe633cb77c
Spaceface16518 <33424649+Spaceface16518@users.noreply.github.com>
2018-01-10 22:13:23 -0600

    Added support for Write module

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+  import Write from "https://gist.github.com/Spaceface16518/f91d67bf0745cead14dc6f81d015e187.js";
   function fetchPython() {
     $.ajax({
       type: "POST",
