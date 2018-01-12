0246820e023ae12416f3027b4f9c74f0ee14582b
Spaceface16518 <33424649+Spaceface16518@users.noreply.github.com>
2018-01-11 20:25:37 -0600

    Added sqlite3-node file skeleton
    This skeleton includes the open and close connection statements, import statements, and the wrapper for the database queries.

--- find-strat.node.js
+++ find-strat.node.js
@@ -1,1 +1,18 @@
+const sqlite3 = require('sqlite3');
+let db = new sqlite3.Database('./data/strats.db', sqlite3.OPEN_READONLY, (err) => {
+  if (err) {
+    console.error(err);
+  }
+  console.log("Connected to the Strats Database")
+});
 
+db.serialize(() => {
+  // Database queries go here
+})
+
+db.close((err) => {
+  if (err) {
+    console.error(err.message);
+  }
+  console.log('Disconnected from the Strats Database');
+});
