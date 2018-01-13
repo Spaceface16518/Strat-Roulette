ea11214ab8791a9fa57a0f324989be8e7517b2ea
Spaceface16518 <33424649+Spaceface16518@users.noreply.github.com>
2018-01-13 01:32:02 -0600

    Initial fetch function completed

--- modules/Fetch.js
+++ modules/Fetch.js
@@ -1,13 +1,23 @@
-module.exports.fetchStrat = (id) => {
+module.exports.fetchStrat = (paramID) => {
   const sqlite3 = require('sqlite3');
+  let id = paramID // id(variable) = id(parameter)
   let db = new sqlite3.Database('../data/strats.db', sqlite3.READ_ONLY, (err) => {
     if (err) {
       console.error(err);
     }
     console.log("Connected to the Strats Database")
   });
-  db.serialize(() => {
-    // Database queries go here
+
+  db.serialize((id) => {
+db.get('SELECT * FROM Strats WHERE id === $ID_place_holder',
+{
+  $ID_place_holder = id
+}, (err, rows) => {
+  if (err) {
+    console.error(err);
+  }
+  return rows;
+})
   })
 
   db.close((err) => {
