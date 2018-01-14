1ee611eaf69bfe88f625caa9c69c3e1d1ed49a6f
Spaceface16518 <33424649+Spaceface16518@users.noreply.github.com>
2018-01-14 16:42:15 -0600

    Write module: added and exported clear function
    - also fixed log statement in write function

--- modules/Write.js
+++ modules/Write.js
@@ -1,12 +1,17 @@
 write = (text) => {
   $(".write-WriteTo").text(text);
-  console.log("\"" + text + "\" " + "was written on the page");
+  console.log("Write: " + "\"" + text + "\" " + "was written on the page");
 }
 testConnection = () => {
   console.log("Connection confirmed: Write")
 }
-
+clear = () => {
+  let html = $(".write-WriteTo").html()
+  $(".write-WriteTo").text("");
+  console.log("Write: " + "Cleared " + html)
+}
 export {
   write,
-  testConnection
+  testConnection,
+  clear
 }
