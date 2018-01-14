44011ae80b3f6a7b4c48530c093581713f70d53e
Spaceface16518 <33424649+Spaceface16518@users.noreply.github.com>
2018-01-14 16:41:19 -0600

    Fixed error message in highest ID module

--- modules/HighestID.js
+++ modules/HighestID.js
@@ -16,7 +16,7 @@ module.exports.getHighestID = () => {
 
   db.close((err) => {
     if (err) {
-      console.error(err.message);
+      console.error(err);
     }
     console.log('Disconnected from the Strats Database');
   });
