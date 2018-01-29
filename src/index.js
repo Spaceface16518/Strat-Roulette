9af9a4dbdf0bbd9b3560fc2c65959621913a1756
Spaceface16518 <33424649+Spaceface16518@users.noreply.github.com>
2018-01-28 20:06:15 -0600

    Add return value to fetch function

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import { generateRandNum, testConnection } from "../assets/modules/RandNum.js";
 const fetch = require("./modules/Fetch.js");
 const highestID = require("./modules/HighestID.js")
 
-
 function fetchStrat() {
   let highestID = highestID.getHighestID();
   let id = generateRandNum(1, highestID);
   let strat = fetch.fetchStrat(id)
+  return strat
 }
 
 function displayStrat(strat) {
